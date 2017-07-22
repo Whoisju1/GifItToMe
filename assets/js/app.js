b59486b454dca56f43cba7459e177bf85619288c
Juan <whoisju1@gmail.com>
2017-07-22 19:38:09 -0400

    develop most of the core funtionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,77 +4,110 @@ let btnContainer = $("#gen-btn-area"),
     input = $("#search-term"),
     searchBtn = $("#search-btn");
 
-let searchTerms = [];
+// when search button is clicked on...
+searchBtn.click(function(e) {
+    e.preventDefault();
 
-searchBtn.click(function () {
-    let searchTerm = input.val().trim();
+    let searchTerms = [];
 
-    // Empty input when search button is clicked
-    input.val("");
+    let searchTerm = input.val().trim().toLowerCase();
 
-    // place search term in searchTerms array
-    searchTerms.push(searchTerm);
+    // if search term is already in array, empty input
+    if (searchTerms.includes(searchTerm)) {
+        input.val("");
+    }
 
-    //empty input box after button is clicked
-    btnContainer.empty();
+    //  && searchTerm.match(/^[0-9a-zA-Z]+$/)
 
-    // iterate through array of search terms and call api for each
-    searchTerms.map(function (term) {
+    // only do a search if input is not empty and is not already in search terms array
+    if (searchTerm && !searchTerms.includes(searchTerm.toLowerCase())) {
+        // Empty input when search button is clicked
+        input.val("");
 
-        let queryURL = `https://api.giphy.com/v1/gifs/search?q=${term}&api_key=dc6zaTOxFJmzC&limit=100`;
+        // place search term in searchTerms array
+        searchTerms.push(searchTerm);
 
-        let gifs = [];
+        //empty input box after button is clicked
+        // btnContainer.empty();
 
-        //Do AJAX call and find gifs
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).done(function (response) {
-            response.data.map(function (obj) {
-                gifs.push(obj);
+        // iterate through array of search terms and call api for each
+        searchTerms.map(function(term, index) {
+            var quantity = 11;
+
+            let queryURL = `https://api.giphy.com/v1/gifs/search?q=${term}&api_key=dc6zaTOxFJmzC&limit=100`;
+
+            let gifs = [];
+
+            //Do AJAX call and find gifs
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).done(function(response) {
+                response.data.map(function(obj) {
+                    gifs.push(obj);
+                });
             });
 
-        });
+            let BtnArea = $(`<div class="btn-area" id="${term}"></div>`);
+            let genBtn = $(`<button class="gen-btn btn btn-area">${term}</button>`).appendTo(BtnArea);
+            let removeBtn = $(`<i class="fa fa-trash-o remove" aria-hidden="true"></i>`).appendTo(BtnArea);
+            console.log('index: ', term);
+            console.log(index);
 
-        let genBtn = $(`<div class="gen-btn"><p>${term}</p></div>`);
+            // when remove button is clicked remove the generated button
+            removeBtn.click(function() {
 
-        genBtn.on('click', function () {
+                let parent = $(this).parent();
+                parent.hide();
+            });
 
-            display.empty();
+            // let parent = $(this).parent();
+            // parent.hide(); 
 
-            // function to display gifs
-            let populate = function () {
-                gifs.map(function (obj, i) {
+            genBtn.on("click", function(e) {
 
-                    if (i < 11) {
-                        // get still and animated images
-                        let stillImg = obj.images.fixed_height_still.url,
-                            animatedImg = obj.images.fixed_height.url;
+                // function to display gifs
+                function populate() {
+                    display.empty();
+                    // empty the display area before it's populated
+                    gifs.map(function(obj, i, gifsArr) {
+                        if (i < quantity) {
+                            // get still and animated images
+                            let stillImg = gifsArr[i].images.fixed_height_still.url,
+                                animatedImg = gifsArr[i].images.fixed_height.url;
 
-                        // create image with attributes
-                        let img = $(`<img 
+                            // create image with attributes
+                            let img = $(`<img 
                             src=${stillImg}
                             data-still${stillImg} 
                             data-animated${animatedImg}
                             class='gif'/>`).appendTo(display);
 
-                        // toggle animation
-                        img.click(function () {
-                            let src = $(this).attr('src');
-                            if (src === stillImg) {
-                                $(this).attr('src', animatedImg);
-                            } else {
-                                $(this).attr('src', stillImg);
-                            }
-                        });
-                    }
+                            // toggle animation and css class
+                            img.click(function() {
+                                $(this).toggleClass("selected").animate("shake");
+                                let src = $(this).attr("src");
+                                if (src === stillImg) {
+                                    $(this).attr("src", animatedImg);
+                                } else {
+                                    $(this).attr("src", stillImg);
+                                }
+                            });
+                        }
+                    });
+                }
 
+                // this is executed when generated btn is clicked
+                populate();
+                $("#show-more").click(function() {
+                    // add 10 more gifs to display area 
+                    quantity += 10;
+                    populate();
                 });
-            };
-            populate();
-        });
+            });
 
-        genBtn.appendTo('#gen-btn-area');
-    });
+            BtnArea.prependTo("#gen-btn-area");
+        });
+    }
 });
 
